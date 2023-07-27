019f71d049e1671c7ba6b7eff901d606f746eb0a
nk0311 <navaitk031@gmail.com>
2023-07-27 00:15:08 -0400

    changes for deployment

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,15 +7,7 @@ import { fadeIn, textVariant } from '../utils/motion';
 
 const Feedbacks = () => {
   return (
-    // <div className="mt-12 bg-black-100 rounded-[20px]">
-    //   <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
-    //     {/* <motion.div variants={textVariant()}>
-    //       <p className={styles.sectionSubText}>What others say</p>
-    //       <h2 className={styles.sectionHeadText}>testimonials</h2>
-    //     </motion.div> */}
-
-    //   </div>
-    // </div>
+   
     <div></div>
   )
 }
